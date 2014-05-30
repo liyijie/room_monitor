bc215b9e108d8c2ecf27a668f2a54b0055ec4ba9
liyijie <liyijie825@gmail.com>
2014-05-30 09:02:58 +0800

    relation bug fix

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,4 @@
 class Area < ActiveRecord::Base
-  has_many :are_arelations
-  has_many :users, through: :are_arelations
+  has_many :area_relations
+  has_many :users, through: :area_relations
 end
